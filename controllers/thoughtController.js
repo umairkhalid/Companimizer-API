6ce09e03f06d484f6545eb7ca2df62beee5ad3e2
umairkhalid <umairkhalid@fastmail.fm>
2022-06-27 22:23:57 +1000

    Create thought function initiated

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,13 +1,36 @@
-const Thought = require("../models/Thought");
+const { Thought, User } = require("../models");
 
 module.exports = {
-    // Get all thoughts
-    getAllThoughts(req, res) {
-      Thought.find()
-        .then((thoughts) => res.json(thoughts))
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-    },
+  // Get all thoughts
+  getAllThoughts(req, res) {
+    Thought.find()
+      .populate({path: 'reactions', select: '-__v'})
+      .sort({_id: -1})
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  // Create a thought
+  createThought(req, res) {
+    console.log("You are adding a thought");
+    console.log(req.body);
+    Thought.create(req.body)
+      .then((thought) =>
+        User.findOneAndUpdate(
+          { _id: req.body.userId },
+          { $addToSet: { thoughts: thought._id } },
+          { runValidators: true, new: true }
+        )
+      )
+      .then((user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: "Thought created but no user with this id!" })
+          : res.json({ message: "Thought sucessfully created!" })
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const {
     getAllThoughts,
+    createThought
 } = require('../../controllers/thoughtController');
 
 // /api/thoughts
 router.route('/')
-.get(getAllThoughts);
+.get(getAllThoughts)
+.post(createThought);
 
 module.exports = router;
